8f3cbee00421b1c250472ee89acd1061a44a23d2
wangzhi <1303249523@qq.com>
2020-07-30 23:18:37 +0800

    添加自我检测，检测结果页面

--- pages/gestate_type/gestate_type.vue
+++ pages/gestate_type/gestate_type.vue
@@ -1,30 +1,23 @@
 <template>
 	<view class="typeBox">
 		<radio-group class="block radioBox" @change="RadioChange">
-			<view class="radioItemBox">
-				<view>
-					<view>孕育状态</view>
-					<text>孕32周</text>
+			<view class="radioItemBox" v-for="item in 3" :key="item">
+				<view class="station"></view>
+				<view class="itemLeft">
+					<view class="title">孕育状态</view>
+					<view class="text">孕32周</view>
 				</view>
-				<view>
+				<view class="itemRight">
 					<view class="title">当前状态</view>
-					<radio :class="radio=='1'?'checked':''" :checked="radio=='1'?true:false" value="1"></radio>
+					<radio :class="radio==item?'checked':''" :checked="radio==item?true:false" :value="item"></radio>
 				</view>
 				
 			</view>
-			<view class="cu-form-group margin-top">
-				<view class="title">单选操作(radio)</view>
-				<radio :class="radio=='2'?'checked':''" :checked="radio=='2'?true:false" value="2"></radio>
-			</view>
-			<view class="cu-form-group margin-top">
-				<view class="title">单选操作(radio)</view>
-				<radio :class="radio=='3'?'checked':''" :checked="radio=='3'?true:false" value="3"></radio>
-			</view>
-			<view class="cu-form-group margin-top">
-				<view class="title">单选操作(radio)</view>
-				<radio :class="radio=='4'?'checked':''" :checked="radio=='4'?true:false" value="4"></radio>
-			</view>
+			
 		</radio-group>
+		<view class="btnBox">
+			<view class="btn">切换</view>
+		</view>
 	</view>
 </template>
 
@@ -51,16 +44,80 @@ export default {
 
 <style lang="scss">
 page{
+	
+}
+.typeBox{
+	height: 100vh;
 	background: linear-gradient(#ffa7c8,#ff75a9);
+	
+	.btnBox{
+		position: fixed;
+		width: 100%;
+		bottom: 20upx;
+	}
+	.btn{
+		width: 90%;
+		height: 80upx;
+		background: #3fad54;
+		border-radius: 10upx;
+		margin: 0 auto;
+		text-align: center;
+		line-height: 80upx;
+		color: #FFF;
+		font-size: 30upx;
+	}
 }
 
 .radioBox{
 	background: transparent;
 	box-sizing: border-box;
+	height: 80vh;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-around;
+	
 }
 .radioItemBox{
 	display: flex;
-	width: 80%;
+	width: 90%;
+	height: 230upx;
 	margin: 0 auto;
+	background-color: $uni-bg-pink;
+	padding:20upx;
+	box-sizing: border-box;
+	color:#FFF;
+	align-items: center;
+	
+	
+	.station{
+		flex:2;
+	}
+	
+	.itemLeft{
+		flex:4;
+		
+		.text{
+			font-size: 26upx;
+			margin-top: 10upx;
+		}
+		
+		.title{
+			font-size: 34upx;
+			font-weight: bold;
+		}
+	}
+	.itemRight{
+		flex:3;
+		
+		display: flex;
+		align-items: center;
+		justify-content: flex-end;
+		
+		
+		.title{
+			margin-right: 20upx;
+			font-size: 26upx;
+		}
+	}
 }
 </style>
---
+++ pages/self_testing/self_testing.vue
@@ -0,0 +1,102 @@
+<template>
+	<view class="testingBox">
+		<ul>
+			<li v-for="(item,index) in questionArr" :key="index">
+				<view class="title">1.孕妇应情绪稳定、心情愉快</view>
+				<view>
+					<radio-group class="block radioBox" @change="RadioChange">
+						<view class="radioItem">
+							<radio :class="1==item.radio?'checked':''" :checked="1==item.radio?true:false" value="1"></radio>
+							<view class="tig">是</view>
+						</view>
+						<view class="radioItem">
+							<radio :class="2==item.radio?'checked':''" :checked="2==item.radio?true:false" value="2"></radio>
+							<view class="tig">否</view>
+						</view>
+					</radio-group>
+				</view>
+			</li>
+		</ul>
+		<view class="btnBox">
+			<view class="btn">提交</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				questionArr:[
+					{title:"孕妇应情绪稳定、心情愉快",radio:"1"},
+					{title:"孕妇应情绪稳定、心情愉快",radio:"1"},
+					{title:"孕妇应情绪稳定、心情愉快",radio:"1"},
+					{title:"孕妇应情绪稳定、心情愉快",radio:"1"},
+				]
+			};
+		},
+		methods:{
+			RadioChange(index){
+				console.log(index);
+				
+			},
+		}
+	}
+</script>
+
+<style lang="scss">
+.testingBox{
+	padding:20upx;
+	box-sizing: border-box;
+	
+	ul{
+		padding-bottom: 100upx;
+		li{
+			height: 280upx;
+			background-color: #FFF;
+			border-radius: 20upx;
+			margin:20upx 0;
+			box-sizing: border-box;
+			
+			.title{
+				padding:20upx;
+				border-bottom: 1px solid #EEE;
+			}
+			
+			.radioBox{
+			}
+			
+			.radioItem{
+				display: flex;
+				padding:20upx;
+				align-items: center;
+				
+				
+				.tig{
+					margin-left: 30upx;
+				}
+			}
+		}
+	}
+	
+	
+	.btnBox{
+		position: fixed;
+		width: 100%;
+		bottom: 20upx;
+		box-sizing: border-box;
+		left: 0;
+		padding: 0 20upx;
+	}
+	.btn{
+		width: 100%;
+		height: 80upx;
+		background: $uni-bg-pink;
+		border-radius: 10upx;
+		text-align: center;
+		line-height: 80upx;
+		color: #FFF;
+		font-size: 30upx;
+	}
+}
+</style>
