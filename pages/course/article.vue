8c112040a62029850bd7410723e94531fb58ade3
wangzhi <1303249523@qq.com>
2020-08-05 17:58:37 +0800

    添加文章

---
+++ pages/course/article.vue
@@ -0,0 +1,132 @@
+<template>
+	<view class="articleBox">
+		<view class="bgBox"></view>
+		<view class="title">数字货币交易所还是一门好生意么？</view>
+		<view class="subTitle">
+			<view class="margin-right">熊猫侠讲书人</view>
+			<view class="margin-right">杂说</view>
+			<view class="timeBox">11-21 07:30</view>
+		</view>
+		<view class="xian"></view>
+		<view class="content"></view>
+		<view>
+			<view>推荐阅读</view>
+			<ul>
+				<li class="flex">
+					<view class="">
+						<view>内容标题摘要标题标题摘要标题摘要</view>
+						<view>文章来源</view>
+					</view>
+					<view class="imgBox"></view>
+				</li>
+			</ul>
+			<view>更多推荐内容</view>
+		</view>
+		<view class="footerBox">
+			<i class="icon_edit"></i>
+			<view class="inputBox"><input type="text" placeholder="写评论"></view>
+			<view class="flex">
+				<i class="icon_chat"></i>
+				<text>9</text>
+			</view>
+			<i class="icon_like"></i>
+			<i class="icon_collect"></i>
+		</view>
+	</view>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+	data() {
+		return {};
+	},
+	computed: {
+		...mapState([])
+	},
+	mounted() {},
+	methods: {},
+	onReachBottom() {}
+};
+</script>
+
+<style lang="scss">
+	.articleBox{
+		padding:20upx 32upx 80upx;
+		
+		.bgBox{
+			width: 100%;
+			height: 100vh;
+			background-color: #FFF;
+			position: fixed;
+			z-index: -1;
+			top:0;
+			left:0;
+		}
+		
+		.title{
+			font-size: 38upx;
+			font-weight: bold;
+		}
+		
+		.subTitle{
+			display: flex;
+			color:#1677FF;
+			margin-top:14upx;
+			font-size: 28upx;
+			
+			.timeBox{
+				color: #999999;
+			}
+		}
+		
+		.xian{
+			width: 200upx;
+			height: 1px;
+			background-color: #EEE;
+			margin:40upx 0upx;
+			display: flex;
+			align-items: center;
+			
+			&::after{
+				content: "";
+				display: block;
+				width: 16upx;
+				height: 16upx;
+				border:1px solid #EEE;
+				transform: rotate(45deg);
+				background-color: #FFF;
+			}
+		}
+		
+		
+		.footerBox{
+			position: fixed;
+			bottom: 0;
+			left: 0;
+			display: flex;
+			width: 100%;
+			height: 100upx;
+			border-top:2upx solid #EEE;
+			justify-content: space-between;
+			align-items: center;
+			padding:0 20upx;
+			box-sizing: border-box;
+			
+			.inputBox{
+				flex:1;
+				font-size: 26upx;
+			}
+
+			
+			i{
+				display: block;
+				width: 40upx;
+				height: 40upx;
+				background:pink;
+				margin:0 0.2rem;
+			}
+		}
+	}
+</style>
--- pages/course/course_info.vue
+++ pages/course/course_info.vue
@@ -2,14 +2,13 @@
 	<view class="courseInfoBox">
 		<view class="videoBox"></view>
 		<view class="tigs margin-bottom">疫情当前，需要推迟产检吗？</view>
-		
+
 		<view class="infoBox">
 			<view class="titleBox">
-				<view :class="type==1?'active':''">课程介绍</view>
-				<view :class="type==2?'active':''">课程目录</view>
+				<view :class="type == 1 ? 'active' : ''">课程介绍</view>
+				<view :class="type == 2 ? 'active' : ''">课程目录</view>
 			</view>
-			
-			
+
 			<view class="introduce">
 				<view class="title">专家介绍</view>
 				<view class="specialist">
@@ -22,14 +21,13 @@
 						<view>北京玛丽妇婴医院。对产产科保健、各项产检检查， 以及高危分娩诊治有丰富的经验。从事妇产临床…</view>
 					</view>
 				</view>
-				
 			</view>
 			<!-- 课程介绍 -->
-			<view class="adBox" v-if="type==1"></view>
+			<view class="adBox" v-if="type == 1"></view>
 			<!-- 课程目录 -->
 			<view class="catalog">
 				<ul>
-					<li>
+					<li class="active">
 						<view class="leftBox">
 							<view class="num">1</view>
 							<view>
@@ -45,6 +43,7 @@
 							<i class="icon"></i>
 							<text>播放</text>
 						</view>
+						<view class="lastTime">上次学到</view>
 					</li>
 				</ul>
 			</view>
@@ -58,7 +57,7 @@ import { mapState } from 'vuex';
 export default {
 	data() {
 		return {
-			type:2,
+			type: 2
 		};
 	},
 	computed: {
@@ -71,143 +70,162 @@ export default {
 </script>
 
 <style lang="scss">
-	.courseInfoBox{
-		.videoBox{
-			width: 100%;
-			height: 422upx;
-			background-color: $uni-bg-pink;
+.courseInfoBox {
+	.videoBox {
+		width: 100%;
+		height: 422upx;
+		background-color: $uni-bg-pink;
+	}
+	.tigs {
+		padding: 32upx;
+		background-color: #fff;
+		font-size: 28upx;
+	}
+
+	.infoBox {
+		background-color: #fff;
+		.titleBox {
+			display: flex;
+			justify-content: space-around;
+			border-bottom: 2upx solid #eee;
+			height: 84upx;
+			align-items: center;
+
+			& > view {
+				height: 100%;
+				line-height: 84upx;
+			}
+			.active {
+				color: $uni-bg-pink;
+				position: relative;
+				font-weight: bold;
+
+				&::after {
+					content: '';
+					display: block;
+					position: absolute;
+					width: 90%;
+					height: 4upx;
+					background-color: $uni-bg-pink;
+					bottom: 0;
+					left: 5%;
+				}
+			}
 		}
-		.tigs{
-			padding:32upx;
-			background-color: #FFF;
-			font-size: 28upx;
+	}
+
+	.introduce {
+		.title {
+			padding: 10upx 32upx;
 		}
-		
-		.infoBox{
-			background-color: #FFF;
-			.titleBox{
+
+		.specialist {
+			display: flex;
+			padding: 32upx;
+
+			.cu-avatar {
+				flex-shrink: 0;
+			}
+
+			.textInfo {
+				font-size: 24upx;
+				color: #989898;
+			}
+			.nameBox {
+				margin-bottom: 10upx;
+			}
+			.name {
+				font-size: 30upx;
+				color: #333333;
+			}
+		}
+	}
+	.adBox {
+		width: 100%;
+		height: 500upx;
+		background-color: pink;
+	}
+
+	.catalog {
+		ul {
+			border-top: 1px solid #eee;
+			position: relative;
+
+			li {
 				display: flex;
-				justify-content: space-around;
-				border-bottom: 2upx solid #EEE;
-				height: 84upx;
-				align-items: center;
-				
-				&>view{
-					height: 100%;
-					line-height: 84upx;
+				justify-content: space-between;
+				padding: 20upx;
+				position: relative;
+
+				.icon {
+					display: inline-block;
 				}
-				.active{
-					color: $uni-bg-pink;
-					position: relative;
-					font-weight: bold;
-					
-					&::after{
-						content: "";
-						display: block;
-						position: absolute;
-						width:90%;
-						height: 4upx;
-						background-color: $uni-bg-pink;
-						bottom: 0;
-						left:5%;
-					}
-				}
-			}
-			
-		}
-		
-		.introduce{
-			.title{
-				padding:10upx 32upx;
-				
-			}
-			
-			.specialist{
-				display: flex;
-				padding:32upx;
-				
-				.cu-avatar{
-					flex-shrink: 0;
-				}
-				
-				.textInfo{
-					font-size: 24upx;
-					color: #989898;
-				}
-				.nameBox{
-					margin-bottom: 10upx;
-				}
-				.name{
-					font-size: 30upx;
-					color: #333333;
-				}
-			}
-			
-		}
-		.adBox{
-			width: 100%;
-			height: 500upx;
-			background-color: pink;
-		}
-		
-		.catalog{
-			ul{
-				li{
+
+				.leftBox {
 					display: flex;
-					justify-content: space-between;
-					padding:20upx;
-					
-					.icon{
-						display: inline-block;
+					align-items: center;
+					color: #989898;
+					font-size: 24upx;
+
+					.title {
+						font-size: 30upx;
+						color: #333;
+						margin-bottom: 10upx;
 					}
-					
-					.leftBox{
+					.timeBox {
 						display: flex;
 						align-items: center;
-						color: #989898;
-						font-size: 24upx;
-						
-						.title{
-							font-size: 30upx;
-							color: #333;
-							margin-bottom: 10upx;
-						}
-						.timeBox{
-							display: flex;
-							align-items: center;
-						}
-						
-						.icon{
-							width: 30upx;
-							height: 30upx;
-							background-color: pink;
-							margin-right: 0.3rem;
-						}
-						
-						.num{
-							padding:10upx 20upx;
-							flex-shrink: 0;
-							font-size: 30upx;
-						}
 					}
+
+					.icon {
+						width: 30upx;
+						height: 30upx;
+						background-color: pink;
+						margin-right: 0.3rem;
+					}
+
+					.num {
+						padding: 10upx 20upx;
+						flex-shrink: 0;
+						font-size: 30upx;
+					}
+				}
+
+				.rightBox {
+					align-self: flex-end;
+					font-size: 24upx;
+					color: #989898;
+					display: flex;
+					align-items: flex-end;
+
+					.icon {
+						width: 50upx;
+						height: 50upx;
+						background-color: pink;
+						margin-right: 0.3rem;
+					}
+				}
+
+				.lastTime {
+					position: absolute;
+					top: 0;
+					right: 40upx;
+					font-size: 24upx;
+					background-color: #f76045;
+					color: #fff;
+					padding: 2upx 4upx;
+					border-bottom-left-radius: 0.2rem;
+					border-bottom-right-radius: 0.2rem;
+				}
+
+				&.active {
 					
-					.rightBox{
-						align-self: flex-end;
-						font-size: 24upx;
-						color: #989898;
-						display: flex;
-						align-items: flex-end;
-						
-						.icon{
-							width: 50upx;
-							height: 50upx;
-							background-color: pink;
-							margin-right: 0.3rem;
-						}
+					.title {
+						color: #f76045;
 					}
 				}
 			}
 		}
 	}
-	
+}
 </style>
